981fccb3e9406441144968efeeabd8df8f103438
Alessandro Calvera <alessandro.calvera@gmail.com>
2023-02-19 21:06:48 -0500

    made changes to

---
+++ index.js
@@ -0,0 +1,54 @@
+function add(){
+
+}
+
+function subtract () {
+
+}
+
+function multiply() {
+
+}
+
+function divide () {
+
+}
+
+function add(a, b) {
+    return (a + b);
+}
+
+function subtract(a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+     return (a * b);
+}
+
+function divide(a, b) {
+    return (a / b);
+}
+
+function increment(n) {
+    return (n + 1);
+}
+
+function decrement(n) {
+    return (n -= 1)
+}
+
+function makeInt(n) {
+    return (n);
+}
+
+function makeInt(n) {
+    toString = parseInt(n, 10);
+    return toString;
+}
+
+
+function preserveDecimal(n) {
+    return (parseFloat(n, 2.222));
+}
+
